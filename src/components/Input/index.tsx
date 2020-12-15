e2fc43fb31b326661f7d45eec031b9380dfae073
Juangomes07 <72666763+Juangomes07@users.noreply.github.com>
2020-12-15 15:56:57 -0300

    Autenticação e cadastro
    Feito a autenticação estilização dos input, conforme o erro ( ficar com borda vermelha).

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,7 @@
-import React, { useEffect, useRef } from 'react';
+import React, {
+  useState, useEffect,
+  useCallback, useRef, useImperativeHandle, forwardRef,
+} from 'react';
 import { TextInputProps } from 'react-native';
 import { useField } from '@unform/core';
 
@@ -12,13 +15,33 @@ interface InputValueReference{
   value: string;
 }
 
-const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
+interface InputRef{
+  focus():void;
+}
+
+const Input: React.RefForwardingComponent<InputRef, InputProps> = (
+  { name, icon, ...rest },
+  ref,
+) => {
   const inputElementRef = useRef<any>(null);
 
   const {
     registerField, defaultValue = '', fieldName, error,
   } = useField(name);
   const inputValueRef = useRef<InputValueReference>({ value: '' });
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputValueRef.current.value);
+  }, []);
 
   useEffect(() => {
     registerField<string>({
@@ -36,15 +59,23 @@ const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
     });
   }, [fieldName, registerField]);
 
+  useImperativeHandle(ref, () => ({
+    focus() {
+      inputElementRef.current.focus();
+    },
+  }));
+
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360" />
+    <Container isFocused={isFocused} isErrored={!!error}>
+      <Icon name={icon} size={20} color={isFocused || isFilled ? '#ff9000' : '#666360'} />
 
       <TextInput
         ref={inputElementRef}
         keyboardAppearance="dark"
         placeholderTextColor="#666360"
         defaultValue={defaultValue}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         onChangeText={(value) => {
           inputValueRef.current.value = value;
         }}
@@ -54,4 +85,4 @@ const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
   );
 };
 
-export default Input;
+export default forwardRef(Input);
